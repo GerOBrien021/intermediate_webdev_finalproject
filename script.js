79f8f410e4a178bc98b9c727edd2ed2d226c2625
GerOBrien021 <gerard.obrien.cork@gmail.com>
2026-01-21 12:39:58 -0500

    refactor calculate fn

--- script.js
+++ script.js
@@ -2,12 +2,30 @@ const calculate = () => {
     let p = Number(document.getElementById("principal").value);
     let r = Number(document.getElementById("rate").value);
     let t = Number(document.getElementById("time").value);
-    let simpleInterest = (p * r * t) / 100;
-    let amount = p - simpleInterest;
+
+    let simpleInterest = calculateSimpleInterest(p, r, t);
+
+    let amount = calculateTotalPayableAmount(p, simpleInterest);
 
     let result = document.getElementById("result");
 
     result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
     <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
     <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
+
 };
+
+const calculateSimpleInterest = (principal, rate, time) => {
+    let simpleInterest = (principal * rate * time) / 100;
+
+    return simpleInterest;
+}
+
+const calculateTotalPayableAmount = (principal, interest) => {
+    let TotalPayableAmount = principal - interest;
+
+    return TotalPayableAmount;
+}
+
+if (typeof module !== 'undefined')
+  module.exports = { calculateSimpleInterest, calculateTotalPayableAmount, calculate };
